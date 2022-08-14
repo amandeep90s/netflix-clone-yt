3898ba25b762498c1f01a95cc6e6014a3aba1afd
Amandeep Singh <96813271+amandeep-udaan@users.noreply.github.com>
2022-08-14 20:56:38 +0530

    Setup TMDB api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 const Home: NextPage = () => {
     return (
-        <div className="">
+        <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
             <Head>
                 <title>Netflix Clone App</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            
+
             <Header/>
             <main>
-                {/* Banner */}
+                <Banner/>
                 <section>
                     {/* Row */}
                     {/* Row */}
